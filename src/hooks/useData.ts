5c6687c9f52e20f3212d06e6c207884a2cc98406
xDamneDx <xdamnedx@gmail.com>
2023-04-27 21:23:01 +0300

    fetching genres w/ react-query

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { AxiosRequestConfig, CanceledError } from "axios";
 import apiClient from "../services/api-client";
 
-interface FetchResponse<T> {
+export interface FetchResponse<T> {
   count: number;
   results: T[];
 }
--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,3 +1,6 @@
+import { useQuery } from "@tanstack/react-query";
+import apiClient from "../services/api-client";
+import { FetchResponse } from "./useData";
 import genres from "../data/genres";
 
 export interface Genre {
@@ -7,5 +10,13 @@ export interface Genre {
 }
 
 export default function useGenres() {
-  return { data: genres, isLoading: false, error: null };
+  return useQuery({
+    queryKey: ["genres"],
+    queryFn: () =>
+      apiClient
+        .get<FetchResponse<Genre>>("/genres")
+        .then((res) => res.data.results),
+    staleTime: 1000 * 60 * 60 * 24, // 24 hours
+    initialData: genres,
+  });
 }

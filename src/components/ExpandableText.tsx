ff3ce2e020f3fd1957f2aaf6f004a230867109a4
xDamneDx <xdamnedx@gmail.com>
2023-05-16 14:38:16 +0200

    building expandable text exercise

--- src/components/ExpandableText.tsx
+++ src/components/ExpandableText.tsx
@@ -9,6 +9,8 @@ const ExpandableText = ({ children }: Props) => {
   const [expanded, setExpanded] = useState(false);
   const limit = 300;
 
+  if (!children) return null;
+
   if (children.length < limit) return <Text>{children}</Text>;
 
   const summary = expanded ? children : children.substring(0, limit) + "...";

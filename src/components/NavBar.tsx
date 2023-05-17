37ad0e31fbd55857442fdab54f5fa08d20cf3532
xDamneDx <xdamnedx@gmail.com>
2023-05-17 10:30:47 +0200

    fixing the navbar exercise

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,11 +2,14 @@ import { HStack, Image } from "@chakra-ui/react";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
 import logo from "../assets/logo.webp";
+import { Link } from "react-router-dom";
 
 export default function NavBar() {
   return (
     <HStack padding="10px">
-      <Image src={logo} boxSize="50px" />
+      <Link to="/">
+        <Image src={logo} boxSize="50px" objectFit="cover" />
+      </Link>
       <SearchInput />
       <ColorModeSwitch />
     </HStack>
